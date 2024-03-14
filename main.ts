17a6f9fbb2dc48616ed5cbbbfe392cd8f3306ba3
JTRNS <jeroen@trines.info>
2024-03-14 10:31:07 +0100

    Refactor template rendering and update views

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 import { serveDir } from "$std/http/file_server.ts";
-import { TodoView } from "./templates.ts";
+import { createTemplate } from "./templates.ts";
 import { type Todo, todoStore } from "./todo_store.ts";
+import * as todoViews from "./todo_view.tsx";
+
+const template = createTemplate(todoViews);
 
 const pattern = new URLPattern({ pathname: "/todos/:id?" });
 
@@ -25,19 +28,19 @@ async function handleTodoRequest(req: Request, todo: Todo) {
   let formData;
   switch (req.method) {
     case "GET":
-      return TodoView("ListItem", { todo });
+      return template(200, "ListItem", { todo });
     case "PUT":
       formData = await req.formData();
       todo.done = formData.get(todo.id) === "on";
       await todoStore.update(todo.id, todo);
-      return TodoView("ListItem", { todo });
+      return template(200, "ListItem", { todo });
     case "POST":
       formData = await req.formData();
       todo.title = formData.get("title")?.toString() ?? todo.title;
       await todoStore.update(todo.id, todo);
-      return TodoView("ListItem", { todo });
+      return template(200, "ListItem", { todo });
     case "PATCH":
-      return TodoView("EditableItem", { todo });
+      return template(200, "EditableItem", { todo });
     case "DELETE":
       await todoStore.delete(todo.id);
       return new Response(null, { status: 200 });
@@ -50,13 +53,13 @@ async function handleTodoListRequest(req: Request) {
   switch (req.method) {
     case "GET": {
       const todos = await todoStore.getAll();
-      return TodoView("TodoIndex", { todos });
+      return template(200, "TodoIndex", { todos });
     }
     case "POST": {
       const formData = await req.formData();
       const title = formData.get("title")?.toString() ?? "new todo";
       const todo = await todoStore.create(title);
-      return TodoView("NewListItem", { todo });
+      return template(200, "NewListItem", { todo });
     }
     default:
       return new Response(null, { status: 405 });
--- templates.ts
+++ templates.ts
@@ -1,11 +1,11 @@
 import type { VNode } from "preact";
 import { renderToString } from "preact-render-to-string";
-import * as todoViews from "./todo_view.tsx";
 
 // deno-lint-ignore no-explicit-any
 export type Template = Record<string, (...args: any[]) => VNode>;
 
 export type TemplateResponse<T extends Template> = <K extends keyof T>(
+  resp: ResponseInit | number,
   key: K,
   ...args: Parameters<T[K]>
 ) => Response;
@@ -13,11 +13,23 @@ export type TemplateResponse<T extends Template> = <K extends keyof T>(
 export function createTemplate<T extends Template>(
   template: T,
 ): TemplateResponse<T> {
-  return (key, ...args) => {
+  return (resp, key, ...args) => {
+    if (typeof resp === "number") {
+      resp = {
+        status: resp,
+        headers: {
+          "Content-Type": "text/html",
+        },
+      };
+    }
+
+    if ("headers" in resp) {
+      resp.headers = new Headers(resp.headers);
+      resp.headers.set("Content-Type", "text/html");
+    }
+
     return new Response(renderToString(template[key](...args)), {
-      headers: { "Content-Type": "text/html" },
+      ...resp,
     });
   };
 }
-
-export const TodoView = createTemplate(todoViews);
